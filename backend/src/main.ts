504bd46eb2ac0b49691b3a9cbcbccdee23bb7b34
Big-cedar <g-ekoh@leadway-group.com>
2026-03-23 23:58:12 +0100

    feat: add global exception handling filter with Prisma error support

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,14 @@
 import 'dotenv/config';
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, HttpAdapterHost } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const httpAdapterHost = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapterHost));
 
   const config = new DocumentBuilder()
     .setTitle('Stellar-Guilds')
